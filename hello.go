d6cffdcecea1863e2ebaadf8dcdf6e5036be1fa0
Bruno Ferreira <bmibferreira@gmail.com>
2020-10-30 01:54:52 +0000

    new

--- hello.go
+++ hello.go
@@ -12,7 +12,7 @@ func main() {
 
 	args := os.Args[1:]
 
-	fmt.Println(fmt.Sprintf("hello %s", args))
+	fmt.Println(fmt.Sprintf("hello %s, is it me?", args))
 	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond) // Build our new spinner
 	s.Start()                                                   // Start the spinner
 	time.Sleep(4 * time.Second)                                 // Run for some time to simulate work
