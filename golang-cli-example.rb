9b0c62a3004a0a223422212e78ad347b567a086f
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-30 00:13:40 +0000

    Brew formula update for golang-cli-example version v0.0.10-alpha.10

--- golang-cli-example.rb
+++ golang-cli-example.rb
@@ -6,12 +6,12 @@ class GolangCliExample < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bmbferreira/golang-cli-example/releases/download/v0.0.10-alpha.10/golang-cli-example-0.0.10-alpha.10-darwin-amd64.tar.gz", :using => GitDownloadStrategy
-    sha256 "c8268866f89d19b26c2309510f1cde7bad5e0f34909b6021cd10e0a3c4d1cd66"
+    url "https://github.com/bmbferreira/golang-cli-example/releases/download/v0.0.10-alpha.10/golang-cli-example_0.0.10-alpha.10_darwin_amd64.tar.gz"
+    sha256 "ef405765ab998b04d22dd4d7c8a596779cecf89ad9cd9f8851b0ed4d42e937c7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bmbferreira/golang-cli-example/releases/download/v0.0.10-alpha.10/golang-cli-example-0.0.10-alpha.10-linux-amd64.tar.gz", :using => GitDownloadStrategy
-      sha256 "bb1ba3e811254fd7db55554bee09bd65b4775e73561e7d6b5b01dd5a85292a45"
+      url "https://github.com/bmbferreira/golang-cli-example/releases/download/v0.0.10-alpha.10/golang-cli-example_0.0.10-alpha.10_linux_amd64.tar.gz"
+      sha256 "2d8cb99752c94cb156c53984519ed7135e016347caf0b05ba26e71d91a4b7167"
     end
   end
 
